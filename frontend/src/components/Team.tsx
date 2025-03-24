6bc087673caa1be83a3170da654cc7cb7343c3d8
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-03-24 18:53:49 +0530

    changes

--- frontend/src/components/Team.tsx
+++ frontend/src/components/Team.tsx
@@ -1,16 +1,41 @@
 // components/Teams.tsx
 import Link from "next/link";
+import Image from "next/image";
 
 const Team = () => {
   return (
     <section className="py-6 bg-gray-100">
-      <h2 className="text-center text-2xl font-bold">🏏 IPL Teams</h2>
-      <div className="flex justify-center space-x-6 mt-4">
-        <Link href="/teams" className="font-semibold hover:text-blue-500">Mumbai Indians</Link>
-        <Link href="/teams" className="font-semibold hover:text-blue-500">Chennai Super Kings</Link>
-        <Link href="/teams" className="font-semibold hover:text-blue-500">RCB</Link>
-        <Link href="/teams" className="font-semibold hover:text-blue-500">KKR</Link>
-      </div>
+      <h2 className="text-center text-2xl font-bold text-blue-500">🏏 IPL Teams</h2>
+     <div className="grid grid-cols-2 md:grid-cols-4 gap-6 m-4">
+  <div className="flex flex-col items-center  shadow-2xl p-4 rounded-lg bg-white">
+    <Link href="/teams" className="font-semibold hover:text-blue-500 text-center">
+      <Image src="/MI.png" alt="Mumbai Indians" width={100} height={100} className="mb-2 h-36 w-40"/>
+      Mumbai <br/> Indians
+    </Link>
+  </div>
+
+  <div className="flex flex-col items-center bg-gray-100 shadow-2xl p-4 rounded-lg  bg-white">
+    <Link href="/teams" className="font-semibold hover:text-blue-500 text-center">
+      <Image src="/csk.png" alt="Chennai Super Kings" width={100} height={100} className="mb-2 h-36 w-36"/>
+      Chennai Super <br/> Kings
+    </Link>
+  </div>
+
+  <div className="flex flex-col items-center bg-gray-100 shadow-2xl p-4 rounded-lg  bg-white">
+    <Link href="/teams" className="font-semibold hover:text-blue-500 text-center">
+      <Image src="/RCB.jpeg" alt="Royal Challengers Bangalore" width={120} height={100} className="mb-2 h-36 w-36"/>
+      Royal Challengers <br/>Bangalore
+    </Link>
+  </div>
+
+  <div className="flex flex-col items-center bg-gray-100 shadow-2xl p-4 rounded-lg  bg-white">
+    <Link href="/teams" className="font-semibold hover:text-blue-500 text-center">
+      <Image src="/SRH.jpeg" alt="Kolkata Knight Riders" width={100} height={100} className="mb-2 h-36 w-36"/>
+      Sunrisers <br/> Hydrabad
+    </Link>
+  </div>
+</div>
+
     </section>
   );
 };
